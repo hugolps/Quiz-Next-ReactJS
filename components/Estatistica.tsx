e55a9455d86394828eea3f395bc913034c1da626
Hugo <hugolps@gmail.com>
2022-03-28 17:10:03 -0300

    Primeiro Commit

---
+++ components/Estatistica.tsx
@@ -0,0 +1,25 @@
+import styles from '../styles/Estatistica.module.css'
+
+interface EstatisticaProps {
+    valor: any
+    texto: string
+    corFundo?: string
+    corFonte?: string
+}
+
+export default function Estatistica(props: EstatisticaProps) {
+    return (
+        <div className={styles.estatistica}>
+            <div className={styles.valor} style={{
+                backgroundColor: props.corFundo ?? '#FDD606',
+                color: props.corFonte ?? '#333'
+
+            }}>
+                {props.valor}    
+            </div>
+            <div className={styles.texto}>
+                {props.texto}    
+            </div>
+        </div>
+    )
+}
